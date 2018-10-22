bcb70ecd72758d68b9a0eff035e033d9fed640dd
Andrés Largo <felipeflogxd@gmail.com>
2018-10-22 18:55:04 +0200

    Añadiendo landing page

--- app/app/assets/javascripts/application.js
+++ app/app/assets/javascripts/application.js
@@ -14,3 +14,15 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
+$(document).ready(function () {
+    $("header .barraFixed").attr('class', 'barra');
+    $("header .barraFixed").fadeOut(1);
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 60 && $(window).width() < 987) {
+            $("header .barra").attr('class', 'barraFixed');
+        } else {
+            $("header .barraFixed").attr('class', 'barra');
+        }
+    });
+});
