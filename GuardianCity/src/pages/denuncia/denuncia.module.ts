fdfe8e8b609f26200e60b2dbe78d22ccf7ed02f4
orjuela45 <miguel.orjuela45@gmail.com>
2018-11-08 16:28:42 +0100

    pagina de reportes

---
+++ GuardianCity/src/pages/denuncia/denuncia.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { DenunciaPage } from './denuncia';
+import { TranslateModule } from '@ngx-translate/core';
+
+
+@NgModule({
+  declarations: [
+    DenunciaPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(DenunciaPage),
+    TranslateModule.forChild()
+  ],
+})
+export class DenunciaPageModule {}
---
+++ GuardianCity/src/pages/denuncia/denuncia.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { TranslateService } from '@ngx-translate/core';
+import { Api } from '../../providers';
+
+/**
+ * Generated class for the DenunciaPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-denuncia',
+  templateUrl: 'denuncia.html',
+})
+export class DenunciaPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,public translateService: TranslateService,
+  public api: Api) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad DenunciaPage');
+  }
+
+}
