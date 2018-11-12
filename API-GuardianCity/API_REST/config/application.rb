c631e58974fa572fe9c86ce4c4edb9db76c74d2c
Erika Infante <37392918+MonoAncestral@users.noreply.github.com>
2018-11-12 11:30:45 -0500

    Para conectarse con Ionic
    lo siento, a alguien le molesta compartir su conocimiento sobre buenas prácticas conmigo pero necesito actualizar

--- API-GuardianCity/API_REST/config/application.rb
+++ API-GuardianCity/API_REST/config/application.rb
@@ -17,6 +17,13 @@ require "rails/test_unit/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins '*'
+    resource '*', headers: :any, methods: [:get, :post, :options]
+  end
+end
+
 module APIRest
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
