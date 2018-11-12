8498ddf06627189db601b302244cfef118a74b34
teamzz111 <aflr@outlook.com>
2018-11-12 00:07:55 -0500

    Actualizando form con iconos y ng model

--- GuardianCity/src/pages/home/home.module.ts
+++ GuardianCity/src/pages/home/home.module.ts
@@ -2,17 +2,19 @@ import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { TranslateModule } from '@ngx-translate/core';
 import { HomePage } from './home';
-
+import { DenunciaPage } from '../denuncia/denuncia';
 @NgModule({
   declarations: [
     HomePage,
+    DenunciaPage
   ],
   imports: [
     IonicPageModule.forChild(HomePage),
-    TranslateModule.forChild()
+    TranslateModule.forChild(),
   ],
   exports: [
-    HomePage
+    HomePage,
+    DenunciaPage
   ]
 })
 export class HomePageModule {}
